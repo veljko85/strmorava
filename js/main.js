e5af9026190111a28c371b9e2d5de60f482561c3
Velibor Markovic <veljko85@users.noreply.github.com>
2018-07-18 17:23:03 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,9 +3,6 @@ $(document).ready(function(){
 	$(".templates").load("templates/home.html");
 	
 	$(".navBar a").click( function() {
-		
-		
-		$(".templates").show();
 
 		$(".templates").load($(this).attr("href")).hide().fadeIn();
         	return(false);
